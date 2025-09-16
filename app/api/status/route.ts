a75c3a02110d17b37683231333751524ce935e73
Shashi prakash singh <singhshashi09871@gmail.com>
2025-09-16 15:21:35 +0530

    Update route.ts

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -1,5 +1,5 @@
  // import { connectDB } from "@/lib/db";
-// import User from "@/models/User";
+// import User from "@/models/User"; 
 // import { NextRequest, NextResponse } from "next/server";
 // import { verifyClerkJWT } from "@/lib/auth";
 
